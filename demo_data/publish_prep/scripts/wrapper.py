252ffa176665e773dde81041263ba40c5bba7eaa
Tyler Hawks <tylerhawks@gmail.com>
2020-11-30 15:12:43 -0500

    updated wrapper

--- demo_data/publish_prep/scripts/wrapper.py
+++ demo_data/publish_prep/scripts/wrapper.py
@@ -1,84 +1,62 @@
-import numpy as np
 import pandas as pd
 from pathlib import Path
 
-data_path = Path.cwd().parent / "data/Equity June-Sept FINALIZED - JUNE Data.csv"
-data = pd.read_csv(data_path) # Read-in data
 
-#%%
+def prep_old_data(path, county_names=True, FIPS=False):
+    # reads in old format equity data file from path;
+    # extracts column, index, and count info separately;
+    # prepares it for cleaning by creating a multi index;
+    # and some small formatting changes
+    df = pd.read_csv(path, header=None, na_values="-")
+    df = df.dropna(axis=1, how="all")
+    dates = df.iloc[0, 3:].fillna(method="ffill")
+    dates = pd.to_datetime(dates)
+    cats = df.iloc[2, 3:].str.replace("[ /]", "_", regex=True).str.lower().fillna("-")
+    if county_names:
+        index = pd.Index(df.iloc[3:, 0], name = "county_names")
+    elif FIPS:
+        index = pd.Index(df.iloc[3:, 2], name="fips_codes")
+    totals = df.iloc[3:,3:].values
+    multi_cols = pd.MultiIndex.from_frame(pd.DataFrame({'dates': dates, 'categories': cats}))
+    return pd.DataFrame(totals, columns=multi_cols, index=index)
 
-col_names = ['Counties','URL','FIPS','Date','Race/Ethnicity/Totals','Category','Count'] # Set new column name
 
-a = data.columns # Grab existing column names (dates)
-b = data.loc[0] # Grab race/ethnicity label
+def clean_old_data(df):
+    # takes prepped dataframe, stacks it to be long, not wide
+    # filters out total columns
+    long_df = df.stack(level=['dates', 'categories'], dropna=True)
+    cats = long_df.index.levels[2]
+    shorter_cats = list(cats[~cats.str.contains('total')])
+    less_long_df = long_df[long_df.index.get_level_values("categories").isin(shorter_cats)]
+    return less_long_df
 
-#%% Find nan values and swap to dates (in a)
 
-daysInd = []
-count = 0
-count2 = 0
-aPost = list(a)
-bPost = list(b)
-reTmp = 'nan'
+def main():
+    data_path = Path.cwd().parent / "data/Equity June-Sept FINALIZED - JUNE Data.csv"
+    prepped_data = prep_old_data(data_path)
+    cleaned_data = clean_old_data(prepped_data)
+    cleaned_data.to_csv(Path.cwd().parent / "data/pub_equity_jun-sept.csv")
+    return cleaned_data
 
-for i in a:
-    
-    if i.find('day') != -1:
-        daysTmp = i
-        
-    if i.find('Unnamed') != -1:
-        aPost[count] = daysTmp
-        
-    count += 1
+if __name__ == "__main__":
+    df = main()
 
-for j in b:
-    
-    if str(j).find('c') != -1:
-        reTmp = j
-        
-    if str(j).find('nan') != -1:
-        bPost[count2] =  reTmp
-        
-    count2 += 1
-                
-daysData = aPost[3:] # Get rid of unwanted column titles (leave only date data)
-reData = bPost[3:] # Get rid of unwanted column titles (leave only RE data)
-catData = data.loc[1][3:] # Get rid of unwanted data in front of RE categories (leave only category data)
 
-#%% Rewrite with one for loop, a counter, and use for Counties, URL, FIPS
 
-counties = list(data['Counties'][2:])
-URL = list(data['County/State Website w/ COVID Info'][2:])
-FIPS = list(data['FIPS Codes'][2:])
-countiesData = []
-URLdata = []
-FIPSdata = []
 
-for counterCUF in range(0,len(counties)):
-    
-    countiesTmp = [counties[counterCUF]]*len(daysData)
-    URLtmp = [URL[counterCUF]]*len(daysData)
-    FIPStmp = [FIPS[counterCUF]]*len(daysData)
-    
-    for counterCUF2 in range(0,len(countiesTmp)):
-        countiesData.append(countiesTmp[counterCUF2])
-        URLdata.append(URLtmp[counterCUF2])
-        FIPSdata.append(FIPStmp[counterCUF2])
 
-#%% Same as above section with Date, Race/Ethnicity/Totals, Category, Count, maybe include data here
 
-daysOut = daysData*len(counties)
-reDataOut = reData*len(counties)
-catDataOut = list(catData)*len(counties)
 
-dataCols = list(a)
-countData = data[dataCols[3:]]
-countDataOut = np.asarray(countData[2:]).flatten()
 
-#%% Actual data list of lists same as above section (flatten)
 
-dataOutPrep = np.asarray([countiesData,URLdata,FIPSdata,daysOut,reDataOut,catDataOut,countDataOut]).T
 
-dataOut = pd.DataFrame(dataOutPrep,columns = col_names)
-#%%
-#dataOut.to_csv(Path.cwd().parent / "data/pub_equity_june-sept.csv")
+
+
+
+
+
+
+
+
+
+
