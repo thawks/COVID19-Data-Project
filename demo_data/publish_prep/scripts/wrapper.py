9c77035ecb6a466f5cc853405795ff473bc05e08
Tyler Hawks <tylerhawks@gmail.com>
2020-12-06 17:39:37 -0500

    updated wrapper, includes october + nov historical data burst

--- demo_data/publish_prep/scripts/wrapper.py
+++ demo_data/publish_prep/scripts/wrapper.py
@@ -1,62 +1,123 @@
 import pandas as pd
 from pathlib import Path
+import gspread
+import numpy as np
 
 
-def prep_old_data(path, county_names=True, FIPS=False):
+def read_data(file_name, check_month=str()):
+    # sets up package gspread to read sheets from gdrive
+    # reads to pandas df
+    # checks if worksheet contains multiple sheets,
+    # if true creates a list of dfs
+    gc = gspread.oauth(flow=gspread.auth.console_flow)
+    ss = gc.open(file_name)
+    if len(ss.worksheets()) == 1:
+        worksheet = ss.get_worksheet(0)
+        df = pd.DataFrame(worksheet.get_all_values())
+        return df
+    elif len(ss.worksheets()) > 1:
+        sheets = ss.worksheets()
+        dfs = []
+        for worksheet in sheets:
+            if check_month in worksheet.title:
+                dfs.append(pd.DataFrame(worksheet.get_all_values()))
+        return dfs
+
+
+def prep_old_data(read_data_df, value_start_index=(3, 2)):
     # reads in old format equity data file from path;
     # extracts column, index, and count info separately;
     # prepares it for cleaning by creating a multi index;
     # and some small formatting changes
-    df = pd.read_csv(path, header=None, na_values="-")
-    df = df.dropna(axis=1, how="all")
-    dates = df.iloc[0, 3:].fillna(method="ffill")
+    v_r = value_start_index[0]  # row index of top right value
+    v_c = value_start_index[1]  # column index of top right value
+    county_names = pd.Index(read_data_df.iloc[v_r:, 0], name="county_names")
+    county_names = county_names.dropna()
+    if county_names.duplicated().any():
+        print("Warning: duplicate counties")
+    empty_to_Na_df = read_data_df.replace("", np.NaN)
+    dropped_empty_cols_df = empty_to_Na_df.dropna(axis=1, how="all")
+    dates = dropped_empty_cols_df.iloc[0, v_c:].str.strip().fillna(method="ffill")
     dates = pd.to_datetime(dates)
-    cats = df.iloc[2, 3:].str.replace("[ /]", "_", regex=True).str.lower().fillna("-")
-    if county_names:
-        index = pd.Index(df.iloc[3:, 0], name = "county_names")
-    elif FIPS:
-        index = pd.Index(df.iloc[3:, 2], name="fips_codes")
-    totals = df.iloc[3:,3:].values
-    multi_cols = pd.MultiIndex.from_frame(pd.DataFrame({'dates': dates, 'categories': cats}))
-    return pd.DataFrame(totals, columns=multi_cols, index=index)
+    cats = (
+        dropped_empty_cols_df.iloc[2, v_c:]
+        .str.replace("[ /]", "_", regex=True)
+        .str.lower()
+        .fillna("-")
+    )
+    totals = dropped_empty_cols_df.iloc[v_r:, v_c:].values
+    multi_cols = pd.MultiIndex.from_frame(
+        pd.DataFrame({"dates": dates, "categories": cats})
+    )
+    rv = pd.DataFrame(totals, columns=multi_cols, index=county_names)
+    return rv
 
 
 def clean_old_data(df):
-    # takes prepped dataframe, stacks it to be long, not wide
-    # filters out total columns
-    long_df = df.stack(level=['dates', 'categories'], dropna=False)
-    cats = long_df.index.levels[2]
-    shorter_cats = list(cats[~cats.str.contains('total')])
-    less_long_df = long_df[long_df.index.get_level_values("categories").isin(shorter_cats)]
-    return less_long_df
+    # takes prepped dataframe, filters out total columns
+    # removes empty rows
+    # stacks it to be long, not wide
+    # removes empty rows and duplicate counties
+    basic_cols = ~df.columns.get_level_values("categories").str.contains("total")
+    simpler_df = df.loc[:, basic_cols]
+    not_empty = [(~row.isna().all()) for index, row in simpler_df.iterrows()]
+    full_df = simpler_df[not_empty]
+    stacked_df = full_df.stack(level=["dates", "categories"])
+    if stacked_df.duplicated().any():
+        merged_df = merge_duplicates(stacked_df)
+        return merged_df
+    else:
+        return stacked_df
+
+
+def merge_duplicates(df, na_values=("0", "-")):
+    # takes stacked_df, finds duplicated indices
+    # checks if matching indices have same values, keeps one if true
+    # if not same, rejects data, prints indices for review
+    noDups = df[df.index.drop_duplicates(keep=False)]  # set aside valid entries
+    dups = df[df.index.duplicated(keep=False)]  # identify duplicated indexes
+    keepers = []
+    rejects = []
+    for index, duplicates in dups.groupby(level=[0, 1, 2]):
+        if (duplicates == duplicates.iloc[0]).all():
+            keepers.append(duplicates.drop_duplicates(keep="first"))
+        else:
+            rejects.append(duplicates)
+    rv = pd.concat([noDups, *keepers])
+    if len(rejects) != 0:
+        rejects = pd.concat(rejects)
+        print(
+            "Warning, duplicate entries for these values: \n",
+            rejects,
+            "\n Above entries rejected.",
+        )
+    elif rv.index.duplicated().any():
+        print(rv.index[rv.index.duplicated()])
+        raise Exception("Merge failed, duplicates remaining")
+    else:
+        print("Duplicates merged successfully")
+    return rv
 
 
 def main():
-    data_path = Path.cwd().parent / "data/Equity June-Sept FINALIZED - JUNE Data.csv"
-    prepped_data = prep_old_data(data_path)
-    cleaned_data = clean_old_data(prepped_data)
-    cleaned_data.to_csv(Path.cwd().parent / "data/pub_equity_jun-sept.csv")
-    return cleaned_data
+    file_name = "Historical Data Burst (Nov 23, 2020)"
+    dfs = read_data(file_name, "")
 
-if __name__ == "__main__":
-    df = main()
-
-
-
-
-
-
-
-
-
-
-
+    main_file = pd.read_csv(
+        Path.cwd().parent / "data/pub_equity_thru_202010.csv",
+        index_col=[0, 1, 2],
+        parse_dates=True,
+        squeeze=True,
+    )
+    for i in range(0, len(dfs)):
+        prepped_data = prep_old_data(dfs[i], value_start_index=(3,3))
+        cleaned_data = clean_old_data(prepped_data)
+        dfs[i] = cleaned_data
+    new = main_file.append(dfs)
+    new.rename("totals", inplace=True)
+    new.to_csv(Path.cwd().parent / "data/pub_equity_thru_202010_update.csv")
+    return new, dfs, main_file
 
 
-
-
-
-
-
-
-
+# if __name__ == "__main__":
+#     result = main()
